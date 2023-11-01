ada9cbfe15a14fa77f90d30ae14381ad533e4949
Stijn <Stijnvandaelen@outlook.com>
2023-11-01 14:04:19 +0100

    footer change

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer class="wrapper">
-	<p>&copy; 2020 Naam en Naam - Groep XX - studenten bij Curio.</p>
+	<p>&copy; 2023 Stijn van Daelen en Floris Melchers - Groep 23c - studenten bij Curio.</p>
 	<p class="muted">Deze website is gebouwd als schoolopdracht voor het eerste leerjaar van de opleiding <em>Software Developer</em></p>
 </footer>
